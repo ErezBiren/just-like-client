37cb40aed6f670d5b3b9bfe1a5e7a33f842e353d
ErezBiren <biran330@gmail.com>
2021-11-01 21:23:26 +0200

    move hash to index.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import Login from "./components/Auth/Login/Login";
 import SignUp from "./components/Auth/SignUp/SignUp";
 import SignUp2 from "./components/Auth/SignUp2/SignUp2";
 import Home from "./pages/Home";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter basename="Just_Like_Client">
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/signup2">
-          <SignUp2 />
-        </Route>
-      </Switch>
-    </HashRouter>
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/signup">
+        <SignUp />
+      </Route>
+      <Route path="/signup2">
+        <SignUp2 />
+      </Route>
+    </Switch>
   );
 }
 
