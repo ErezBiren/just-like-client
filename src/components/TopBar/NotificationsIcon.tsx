119a974e8ee99590281a054dcaaa7216627002ad
ErezBiren <biran330@gmail.com>
2021-11-13 10:56:20 +0200

    notifications

--- src/components/TopBar/NotificationsIcon.tsx
+++ src/components/TopBar/NotificationsIcon.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { ReactComponent as BellIcon } from "../../assets/dashboard/bell.svg";
 import Badge from "@mui/material/Badge";
 import CircleIcon from "@mui/icons-material/Circle";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 function NotificationsIcon() {
   const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
@@ -20,43 +21,62 @@ function NotificationsIcon() {
   return (
     <div>
       <IconButton onClick={handleClick}>
-        <Badge badgeContent={4} color="secondary">
+        <Badge
+          badgeContent={4}
+          sx={{
+            "& .MuiBadge-badge": {
+              color: "white",
+              backgroundColor: "var(--main-color)",
+            },
+          }}
+        >
           <BellIcon />
         </Badge>
       </IconButton>
 
       <Menu anchorEl={anchorElement} open={open} onClose={handleClose}>
         <div className={classes.root}>
-          <h4>התראות</h4>
+          <div className={classes.header}>
+            <h3>התראות</h3>
+            <MoreHorizIcon />
+          </div>
           <div className={classes.row}>
-            <Avatar sx={{ bgcolor: "#E5BB69" }}>נב</Avatar>
-            <div>
-              <p>שלום שלום</p>
-              <p>לפני 20 דקות</p>
+            <div className={classes.rightSide}>
+              <Avatar sx={{ bgcolor: "#BA11D5" }}>טב</Avatar>
+              <div className={classes.text}>
+                <h4 className={classes.h4}>שלום שלום</h4>
+                <p className={classes.p}>לפני 20 דקות</p>
+              </div>
             </div>
             <CircleIcon fontSize="small" sx={{ color: "var(--main-color)" }} />
           </div>
           <div className={classes.row}>
-            <Avatar sx={{ bgcolor: "#E5BB69" }}>נב</Avatar>
-            <div>
-              <p>שלום שלום</p>
-              <p>לפני 20 דקות</p>
+            <div className={classes.rightSide}>
+              <Avatar sx={{ bgcolor: "#11D548" }}>לכ</Avatar>
+              <div className={classes.text}>
+                <h4 className={classes.h4}>שלום שלום</h4>
+                <p className={classes.p}>לפני 20 דקות</p>
+              </div>
             </div>
             <CircleIcon fontSize="small" sx={{ color: "var(--main-color)" }} />
           </div>
           <div className={classes.row}>
-            <Avatar sx={{ bgcolor: "#E5BB69" }}>נב</Avatar>
-            <div>
-              <p>שלום שלום</p>
-              <p>לפני 20 דקות</p>
+            <div className={classes.rightSide}>
+              <Avatar sx={{ bgcolor: "#E5BB69" }}>יא</Avatar>
+              <div className={classes.text}>
+                <h4 className={classes.h4}>שלום שלום</h4>
+                <p className={classes.p}>לפני 20 דקות</p>
+              </div>
             </div>
             <CircleIcon fontSize="small" sx={{ color: "var(--main-color)" }} />
           </div>
           <div className={classes.row}>
-            <Avatar sx={{ bgcolor: "#E5BB69" }}>נב</Avatar>
-            <div>
-              <p>שלום שלום</p>
-              <p>לפני 20 דקות</p>
+            <div className={classes.rightSide}>
+              <Avatar sx={{ bgcolor: "#699BE5" }}>נב</Avatar>
+              <div className={classes.text}>
+                <h4 className={classes.h4}>שלום שלום</h4>
+                <p className={classes.p}>לפני 20 דקות</p>
+              </div>
             </div>
             <CircleIcon fontSize="small" sx={{ color: "var(--main-color)" }} />
           </div>
