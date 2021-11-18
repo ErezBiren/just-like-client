c3423a18313743ec38d063184420f1f238eef3c2
ErezBiren <biran330@gmail.com>
2021-11-19 01:12:03 +0200

    update the not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import classes from "./NotFoundPage.module.css";
 const NotFoundPage = () => {
-  return <div>not found!!</div>;
+  return (
+    <div className={classes.main}>
+      <h1>אופס!</h1>
+      <h2>מה אתם עושים פה?</h2>
+      <Link to="/">לחזרה למסך הבית</Link>
+    </div>
+  );
 };
 // Todo: Design this page
 
